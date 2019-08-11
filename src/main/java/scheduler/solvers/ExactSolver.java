b42fa5720a9d2192d650480dc85e0e5bcfdd15d1
Zakkari Abdelhakim <the.z.squared@gmail.com>
2019-08-11 01:04:56 +0100

    Greatly improved open-shop solver speed.

--- src/main/java/scheduler/solvers/ExactSolver.java
+++ src/main/java/scheduler/solvers/ExactSolver.java
@@ -20,122 +20,125 @@ public class ExactSolver extends Solver {
     @Override
     protected List<Schedule> solveMakespan(Task currentTask) {
 
-            if (this.getProblem().machineCount == 0) {
-                System.err.println("No matrix set");
-                return new ArrayList<>();
+        if (this.getProblem().machineCount == 0) {
+            System.err.println("No matrix set");
+            return new ArrayList<>();
+        }
+
+        int[][] timeMatrix = getProblem().getTimeMatrix();
+        int[][] machineMatrix = getProblem().getMachineMatrix();
+
+        final List<Schedule> schedules = new ArrayList<>(); //To populate with final solution
+
+        if (getProblem().getType() == ShopType.FLOW) {
+            Schedule[] baseMachines = new Schedule[getProblem().machineCount];
+
+            //Generate schedule per job, note this is iterating vertically
+            for (int i = 0; i < baseMachines.length; i++) {
+                baseMachines[i] = new Schedule();
+                for (int j = 0; j < timeMatrix.length; j++) {
+                    if (timeMatrix[j][i] != 0) {
+                        baseMachines[i].put(new Point(j, i), timeMatrix[j][i]);
+                    }
+                }
             }
 
-            int[][] timeMatrix = getProblem().getTimeMatrix();
-            int[][] machineMatrix = getProblem().getMachineMatrix();
-
-            final List<Schedule> schedules = new ArrayList<>(); //To populate with final solution
-
-            if (getProblem().getType() == ShopType.FLOW) {
-                Schedule[] baseMachines = new Schedule[getProblem().machineCount];
-
-                //Generate schedule per job, note this is iterating vertically
-                for (int i = 0; i < baseMachines.length; i++) {
-                    baseMachines[i] = new Schedule();
-                    for (int j = 0; j < timeMatrix.length; j++) {
-                        if (timeMatrix[j][i] != 0) {
-                            baseMachines[i].put(new Point(j, i), timeMatrix[j][i]);
-                        }
-                    }
-                }
-
-                List<List<Schedule>> baseSchedule = new ArrayList<>();
-                //Permutations of each job's schedule
-                for (Schedule sch : baseMachines) {
-                    baseSchedule.add(permute(sch, sch.size(),currentTask));
-                    if(currentTask.isCancelled()) return null;
-                }
-
-                //Cartesian array combination of schedules together
-                List<List<Schedule>> full = Lists.cartesianProduct(baseSchedule);    //4jobs x 8 machines, 7x3, 5x5
-
-                //Concatenate into final Schedules containing all jobs + machines
-                for (List<Schedule> sch : full) {
-                    schedules.add(Schedule.concat(sch));
-                }
-
-                //Calculate makespan
-                for (Schedule sch : schedules) {
-                    if(currentTask.isCancelled()) return null;
-                    sch.forEach((index, time) -> sch.put(index, time + getPreviousTime(sch, index.x, index.y)));
-                }
-
-
-            } else if (getProblem().getType() == ShopType.OPEN) {
-                Schedule baseSchedule = new Schedule();
-
-                for (int i = 0; i < getProblem().machineCount; i++) {
-                    for (int j = 0; j < timeMatrix.length; j++) {
-                        if (timeMatrix[j][i] != 0) {
-                            baseSchedule.put(new Point(j, i), timeMatrix[j][i]);
-                        }
-                        if(currentTask.isCancelled()) return null;
-                    }
-                }
-
-                schedules.addAll(permute(baseSchedule, baseSchedule.size(),currentTask));
-
-                //Calculate makespan
-                for (Schedule sch : schedules) {
-                    if(currentTask.isCancelled()) return null;
-                    sch.forEach((index, time) -> sch.put(index, time + getPreviousTime(sch, index.x, index.y)));
-                }
-
-
-            } else if (getProblem().getType() == ShopType.JOB) {
-                Schedule[] baseMachines = new Schedule[getProblem().machineCount];
-
-                //Generate schedule per order, note this is iterating vertically
-                for (int i = 0; i < baseMachines.length; i++) {
-                    baseMachines[i] = new Schedule();
-                    for (int j = 0; j < machineMatrix.length; j++) {
-                        if (timeMatrix[j][i] != 0) {
-                            baseMachines[i].put(new Point(j, machineMatrix[j][i]), timeMatrix[j][i]);
-                        }
-                        if(currentTask.isCancelled()) return null;
-                    }
-                }
-
-                List<List<Schedule>> baseSchedule = new ArrayList<>();
-                //Permutations of each job's schedule
-                for (Schedule sch : baseMachines) {
-                    baseSchedule.add(permute(sch, sch.size(),currentTask));
-                    if(currentTask.isCancelled()) return null;
-                }
-
-                //Cartesian array combination of schedules together
-                List<List<Schedule>> full = Lists.cartesianProduct(baseSchedule);    //4jobs x 8 machines, 7x3, 5x5
-
-                //Concatenate into final Schedules containing all jobs + machines
-                for (List<Schedule> sch : full) {
-                    if(currentTask.isCancelled()) return null;
-                    schedules.add(Schedule.concat(sch));
-                }
-
-                //Calculate makespan
-                for (Schedule sch : schedules) {
-                    if(currentTask.isCancelled()) return null;
-                    sch.forEach((index, time) -> sch.put(index, time + getPreviousTime(sch, index.x, index.y)));
-                }
-
+            List<List<Schedule>> baseSchedule = new ArrayList<>();
+            //Permutations of each job's schedule
+            for (Schedule sch : baseMachines) {
+                baseSchedule.add(permute(sch, sch.size(), currentTask));
             }
 
-            return schedules;
-    }
+            //Cartesian array combination of schedules together
+            List<List<Schedule>> full = Lists.cartesianProduct(baseSchedule);    //4jobs x 8 machines, 7x3, 5x5
 
-    //Get the worst end time for the previous task
-    private int getPreviousTime(Schedule schedule, int jobIndex, int machineIndex) {
-        Integer precedent = schedule.get(schedule.getPrecedentJob(new Point(jobIndex, machineIndex)));
-        Integer previous = schedule.get(schedule.getPreviousJob(new Point(jobIndex, machineIndex)));
+            //Concatenate into final Schedules containing all jobs + machines
+            for (List<Schedule> sch : full) {
+                schedules.add(Schedule.concat(sch));
+            }
 
-        precedent = precedent == null ? 0 : precedent;
-        previous = previous == null ? 0 : previous;
+        }
+        else if (getProblem().getType() == ShopType.OPEN) {
+            Schedule[] baseMachines = new Schedule[getProblem().machineCount];
 
-        return Math.max(precedent, previous);
+            //Generate schedule per job, note this is iterating vertically
+            for (int i = 0; i < baseMachines.length; i++) {
+                baseMachines[i] = new Schedule();
+                for (int j = 0; j < timeMatrix.length; j++) {
+                    if (timeMatrix[j][i] != 0) {
+                        baseMachines[i].put(new Point(j, i), timeMatrix[j][i]);
+                    }
+                }
+            }
+
+            List<List<Schedule>> baseSchedule = new ArrayList<>();
+            //Permutations of each job's schedule
+            for (Schedule sch : baseMachines) {
+                baseSchedule.add(permute(sch, sch.size(), currentTask));
+            }
+
+            //Cartesian array combination of schedules together
+            List<List<Schedule>> full = Lists.cartesianProduct(baseSchedule);    //4jobs x 8 machines, 7x3, 5x5
+            List<Schedule> initialPermutation = new ArrayList<>();
+
+            //Concatenate into final Schedules containing all jobs + machines
+            for (List<Schedule> sch : full) {
+                initialPermutation.add(Schedule.concat(sch));
+            }
+
+            //Concatenate into final Schedules containing all jobs + machines
+            for (Schedule sch : initialPermutation) {
+                List<Schedule> machinePerms = new ArrayList<>();
+                machinePerms.add(sch);
+                for (int i = 0; i < timeMatrix.length; i++) {
+                    final List<Schedule> tempPerms = new ArrayList<>();
+                    for (Schedule schedule : machinePerms) {
+                        tempPerms.addAll(permuteSubset(schedule, i, currentTask));
+                    }
+                    machinePerms = tempPerms;
+                }
+                schedules.addAll(machinePerms);
+            }
+        }
+        else if (getProblem().getType() == ShopType.JOB) {
+            Schedule[] baseMachines = new Schedule[getProblem().machineCount];
+
+            //Generate schedule per order, note this is iterating vertically
+            for (int i = 0; i < baseMachines.length; i++) {
+                baseMachines[i] = new Schedule();
+                for (int j = 0; j < machineMatrix.length; j++) {
+                    if (timeMatrix[j][i] != 0) {
+                        baseMachines[i].put(new Point(j, machineMatrix[j][i]), timeMatrix[j][i]);
+                    }
+                    if (currentTask.isCancelled()) return null;
+                }
+            }
+
+            List<List<Schedule>> baseSchedule = new ArrayList<>();
+            //Permutations of each job's schedule
+            for (Schedule sch : baseMachines) {
+                baseSchedule.add(permute(sch, sch.size(), currentTask));
+                if (currentTask.isCancelled()) return null;
+            }
+
+            //Cartesian array combination of schedules together
+            List<List<Schedule>> full = Lists.cartesianProduct(baseSchedule);    //4jobs x 8 machines, 7x3, 5x5
+
+            //Concatenate into final Schedules containing all jobs + machines
+            for (List<Schedule> sch : full) {
+                if (currentTask.isCancelled()) return null;
+                schedules.add(Schedule.concat(sch));
+            }
+
+        }
+
+        //Calculate makespan
+        for (Schedule sch : schedules) {
+            if (currentTask.isCancelled()) return null;
+            sch.forEach((job, time) -> sch.put(job, time + sch.getPreviousTime(job)));
+        }
+
+        return schedules;
     }
 
 }
